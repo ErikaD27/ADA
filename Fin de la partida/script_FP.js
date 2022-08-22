cf6fbb9fc752839f3618b603e1ea3eb18fc8be3d
Erika <erikaduarte2014@hotmail.com-v>
2022-08-22 12:28:00 -0500

    Cmabio de nombres

--- Fin de la partida/script_FP.js
+++ Fin de la partida/script_FP.js
@@ -4,19 +4,18 @@ let tabla = document.getElementById('T1');
     document.getElementById('resultado').innerText = filas.length;
     
 
-function alerta() 
-  {
-    var mensaje;
-    let Cord1 = parseInt(prompt("Ingrese la coordenada 1"));
-    let Cord2 = parseInt(prompt("Ingrese la coordenada 2"));
+function Coordinates() {
+  var mensaje;
+  let Cord1 = parseInt(prompt("Ingrese la coordenada 1"));
+  let Cord2 = parseInt(prompt("Ingrese la coordenada 2"));
     
   if (filas.length >= (Cord2*Cord1)) {
     mensaje = "Has ingresado coordenadas válidas";
   } else {
     mensaje = "Haz ingresado coordenadas inválidas";
-        }
-  document.getElementById("Mensaje").innerHTML = mensaje;
   }
+  document.getElementById("Mensaje").innerHTML = mensaje;
+}
 
 
 
